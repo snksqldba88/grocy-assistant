25dcdbfbbadbfbd574baae78eadc8b4c02cdd830
snksqldba88 <snksqldba88@gmail.com>
2026-09-08 21:59:16 -0400

    Testing file

--- Testing/test_conv_flow.py
+++ Testing/test_conv_flow.py
@@ -1,9 +1,13 @@
+# from assistant.engine import process_message
+#
+# print(process_message("add product"))
+# print(process_message("Cancellation Test Product"))
+# print(process_message("Dal & Legumes"))
+# print(process_message("Shelf"))
+# print(process_message("kilogram"))
+# print(process_message("kilogram"))
+# print(process_message("no"))
+
 from assistant.engine import process_message
 
-print(process_message("add product"))
-print(process_message("Cancellation Test Product"))
-print(process_message("Dal & Legumes"))
-print(process_message("Shelf"))
-print(process_message("kilogram"))
-print(process_message("kilogram"))
-print(process_message("no"))
+print(process_message("help"))
